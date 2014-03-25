25da9cca036a9aac3d8c6bdf49f2769a24ce3bcc
Avinash Krishna Kumar <avi1989@gmail.com>
2014-03-24 20:28:45 -0400

    Initial Page created

---
+++ examples/Knockout.Select2/Knockout.Select2/Scripts/ViewModels/Index.js
@@ -0,0 +1,9 @@
+﻿var IndexVM = function() {
+    var test = ko.observable("Binding Successful");
+    var colorList = ['Blue', 'Red', 'White', 'Green', 'Black', 'Orange'];
+    var vm = {
+        test: test,
+        colorList: colorList
+    };
+    return vm;
+}
